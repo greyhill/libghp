3ba4597180aa0cfa7308975ba351539b521393d9
Madison McGaffin <greyhill@gmail.com>
2010-02-03 18:14:07 -0500

    work...

---
+++ gl.hpp
@@ -0,0 +1,7 @@
+#ifndef _GHP_GL_HPP_
+#define _GHP_GL_HPP_
+
+#include "gl/window.hpp"
+
+#endif
+
---
+++ gl/window.hpp
@@ -0,0 +1,12 @@
+#ifndef _GHP_GL_WINDOW_HPP_
+#define _GHP_GL_WINDOW_HPP_
+
+namespace ghp {
+namespace gl {
+
+
+
+}}
+
+#endif
+
