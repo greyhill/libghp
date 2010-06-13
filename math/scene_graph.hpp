d084b36ed4a1247d6984269d2303ca240addbfdc
Madison McGaffin <greyhill@gmail.com>
2010-06-13 22:28:18 +0200

    1st draft of scene_graph

--- math/scene_graph.hpp
+++ math/scene_graph.hpp
@@ -8,6 +8,11 @@
 
 namespace ghp {
 
+/** \brief hierarchical scene graph
+ \tparam N - dimension of scene (2 or 3)
+ \tparam T - underlying floating point type
+ \tparam C - type of contents
+ */
 template<int N, typename T, typename C>
 class scene_graph {
 public:
@@ -15,7 +20,33 @@ public:
   typedef typename spatial_traits<N, T>::stable_rot_t stable_rot_t;
   typedef typename spatial_traits<N, T>::matrix_rot_t matrix_rot_t;
   typedef typename spatial_traits<N, T>::matrix_trans_t matrix_trans_t;
+  typedef C value_type;
 
+  /** \brief create a new scene_graph object
+    \param loc - location relative to parent
+    \param rot - rotation relative to parent
+   */
+  scene_graph(const vector_t &loc = vector_t(), 
+      const stable_rot_t &rot = stable_rot_t())
+      : loc_(loc), rot_(rot) {
+  }
+  /** \brief copy constructor */
+  scene_graph(const scene_graph &g) {
+    // perform deep copy 
+    (*this) = g;
+  }
+  ~scene_graph() {
+    for(std::set<scene_graph>::const_iterator it=children_.begin();
+        it != children.end(); ++it) {
+      erase_child(**it);
+    }
+  }
+
+  /** \brief "flatten" the local coordinate spaces of the scene graph's
+    hierarchy onto a single, global coordinate space
+      \tparam SG - must have insert(vec_t, stable_rot_t, C)
+      \param out - output scenegraph
+    */
   template<SG>
   inline void flatten(SG &out) const {
     // identity rotation and translation
@@ -24,11 +55,56 @@ public:
     matrix_rot_t rot_matrix;
     flatten_helper_(out, loc, rot, rot_matrix);
   }
+
+  /** \brief element access */
+  inline const vector_t& location() const { return loc_; }
+  /** \brief element access */
+  inline vector_t& location() { return loc_; }
+  /** \brief element access */
+  inline const stable_rot_t& rotation() const { return rot_; }
+  /** \brief element access */
+  inline stable_rot_t& rotation() { return rot_; }
   
-  inline const std::set<C*>& contents() const { return contents_; }
-  inline void add_content(C *c) { contents_.insert(c); }
-  inline void remove_content(C *c) { contents_.erase(c); }
+  /** \brief element access */
+  inline const std::set<C>& contents() const { return contents_; }
+  /** \brief element access */
+  inline void insert(const C &c) { contents_.insert(c); }
+  /** \brief element access */
+  inline void erase(const C &c) { contents_.erase(c); }
+  /** \brief element access */
   inline const std::set<scene_graph*>& children() const { return children_; }
+  /** \brief create a new child node and return a reference to it.
+         the child's memory is owned by this scene graph.
+    \param loc - relative displacement
+    \param rot - relative rotation
+   */
+  inline scene_graph& insert_child(const vector_t &loc=vector_t(), 
+      const stable_rot_t &rot=stable_rot_t()) {
+    scene_graph *new_child = new scene_graph(loc, rot);
+    children_.insert(new_child);
+    return new_child;
+  }
+  /** \brief remove and destroy a child */
+  inline void erase_child(scene_graph &g) {
+    children_.erase(&g);
+    delete g;
+  }
+
+  /** \brief deep copy */
+  template<typename T2>
+  scene_graph& operator=(const scene_graph<N, T2, C> &g) {
+    loc_ = g.loc_;
+    rot_ = g.rot_;
+    contents_ = g.contents_;
+    children_.erase(children_.begin(), children_.end());
+    for(std::set< scene_graph<N, T2, C> >::const_iterator it 
+        = g.contents_.begin(); it != g.contents_.end(); ++it) {
+      scene_graph *g = new scene_graph();
+      *g = *it;
+      children_.insert(g);
+    }
+    return *this;
+  }
 
 private:
   template<SG>
@@ -45,7 +121,7 @@ private:
     // add content
     for(std::set<C*>::const_iterator it=contents_.begin(); 
         it != contents.end(); ++it) {
-      out.add_content(*it, cur_loc, cur_rot);
+      out.insert(*it, cur_loc, cur_rot);
     }
     // add children
     for(std::set<scene_graph*>::const_iterator it = children_.begin();
@@ -56,7 +132,7 @@ private:
 
   vector_t loc_;
   stable_rot_t rot_;
-  std::set<C*> contents_;
+  std::set<C> contents_;
   std::set<scene_graph*> children_;
 };
 
