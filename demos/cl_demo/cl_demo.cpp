b28f7ce2e6c1fec9a94f7e9b880b5ad8a048f16a
Madison McGaffin <greyhill@gmail.com>
2010-09-19 12:28:18 -0400

    much cleaner cl::platform design

--- demos/cl_demo/cl_demo.cpp
+++ demos/cl_demo/cl_demo.cpp
@@ -1,26 +1,13 @@
 #include <ghp/util/cl.hpp>
 
 #include <iostream>
+#include <list>
 
 int main(int argc, char *argv[]) {
-  // display available platforms and devices!
-  const std::list<const cl::platform*> &platforms 
-      = cl::platform::get_platforms();
-  for(std::list<const cl::platform*>::const_iterator it = platforms.begin();
-      it != platforms.end(); ++it) {
-    std::cout << "Platform: " << (*it)->name() << 
-      ", " << (*it)->version() << " from " << (*it)->vendor()
-      << " [" << (*it)->profile() << "] supports extensions: " << std::endl;
+  std::list<cl::platform> platforms;
+  cl::platform::get_platforms(platforms);
 
-    // display supported extensions
-    const std::set<std::string>& extns = (*it)->extensions();
-    for(std::set<std::string>::const_iterator eit = extns.begin();
-        eit != extns.end(); ++eit) {
-      std::cout << *eit << std::endl;
-    }
-    std::cout << std::endl;
-
-//    const std::list<cl::device>& devices = (*it)->devices();
-  }
+  cl::platform p = platforms.front();
+  std::cout << p.name() << std::endl;
 }
 
--- util/cl.hpp
+++ util/cl.hpp
@@ -6,6 +6,7 @@
 #include <boost/noncopyable.hpp>
 #include <boost/shared_array.hpp>
 
+#include <cstdarg>
 #include <list>
 #include <set>
 #include <sstream>
@@ -15,15 +16,99 @@
 
 namespace cl {
 
+/* right now, all parameters return strings */
+template<cl_platform_info I> struct cl_platform_info_retval {
+  typedef std::string value;
+};
 
+template<typename RETVAL> RETVAL platform_get_info_(cl_platform_id id, 
+    cl_platform_info param) {
+  /* use specializations only */
+  assert(false);
+}
+
+template<> std::string platform_get_info_<std::string>(cl_platform_id id, 
+    cl_platform_info param) {
+  std::vector<char> buf;
+  int err;
+  std::size_t size;
+  err = clGetPlatformInfo(id, param, 0, NULL, &size);
+  if(err != CL_SUCCESS) throw std::runtime_error("cl error");
+  buf.reserve(size);
+  err = clGetPlatformInfo(id, param, size, &buf[0], NULL);
+  if(err != CL_SUCCESS) throw std::runtime_error("cl error");
+
+  return std::string(&buf[0]);
+}
+
+template<int UNUSED>
+class platform_ {
+public:
+  inline platform_(cl_platform_id id) 
+      : id_(id) { }
+  inline ~platform_() { }
+
+  template<cl_platform_info PARAM>
+  typename cl_platform_info_retval<PARAM>::value
+  inline get_info() const {
+    return platform_get_info_<typename cl_platform_info_retval<PARAM>::value>
+      (id_, PARAM);
+  }
+
+  template<typename C>
+  static void get_platforms(C &c) {
+    int err;
+    uint32_t num_platforms;
+    std::vector<cl_platform_id> platform_ids;
+    err = clGetPlatformIDs(0, NULL, &num_platforms);
+    if(err != CL_SUCCESS) throw std::runtime_error("cl error");
+    platform_ids.reserve(num_platforms);
+    err = clGetPlatformIDs(num_platforms, &platform_ids[0], NULL);
+    if(err != CL_SUCCESS) throw std::runtime_error("cl error");
+
+    for(int i=0; i<num_platforms; ++i) {
+      c.insert(c.end(), platform_(platform_ids[i]));
+    }
+  }
+
+  inline std::string profile() const {
+    return get_info<CL_PLATFORM_PROFILE>();
+  }
+  inline std::string version() const {
+    return get_info<CL_PLATFORM_VERSION>();
+  }
+  inline std::string name() const {
+    return get_info<CL_PLATFORM_NAME>();
+  }
+  inline std::string vendor() const {
+    return get_info<CL_PLATFORM_VENDOR>();
+  }
+  inline std::string extensions() const {
+    return get_info<CL_PLATFORM_EXTENSIONS>();
+  }
+  inline cl_platform_id id() const {
+    return id_;
+  }
+
+private:
+  cl_platform_id id_;
+};
+typedef platform_<0> platform;
+
+template<int UNUSED>
+class device_ {
+};
+typedef device_<0> device;
 
 template<int UNUSED>
 class context_ : boost::noncopyable {
 public:
-  context()
-  ~context();
+  context_(platform p, device d1, ...) { }
+  ~context_();
 
 private:
+  platform platform_;
+  std::vector<device> devices_;
 };
 typedef context_<0> context;
 
