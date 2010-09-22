78e6f32a2f1e64d7d215904be7868a674ecdf1e3
Madison McGaffin <greyhill@gmail.com>
2010-09-21 22:43:16 -0400

    a vastly more satisfying CL API

--- util/cl.hpp
+++ util/cl.hpp
@@ -17,9 +17,22 @@
 
 namespace cl {
 
-template<int UNUSED> class platform_;
-template<int UNUSED> class device_;
-template<int UNUSED> class context_;
+template<int UNUSED> class platform_ref_;
+template<int UNUSED> class device_ref_;
+template<int UNUSED> class context_ref_;
+template<int UNUSED> class event_ref_;
+template<int UNUSED> class command_queue_ref_;
+template<typename T> class buffer_ref;
+template<int UNUSED> class program_ref_;
+template<int UNUSED> class kernel_ref_;
+
+typedef platform_ref_<0> platform_ref;
+typedef device_ref_<0> device_ref;
+typedef context_ref_<0> context_ref;
+typedef event_ref_<0> event_ref;
+typedef command_queue_ref_<0> command_queue_ref;
+typedef program_ref_<0> program_ref;
+typedef kernel_ref_<0> kernel_ref;
 
 /* right now, all parameters return strings */
 template<cl_platform_info I> struct cl_platform_info_retval {
@@ -47,11 +60,11 @@ template<> std::string platform_get_info_<std::string>(cl_platform_id id,
 }
 
 template<int UNUSED>
-class platform_ {
+class platform_ref_ {
 public:
-  inline platform_(cl_platform_id id) 
+  inline platform_ref_(cl_platform_id id) 
       : id_(id) { }
-  inline ~platform_() { }
+  inline ~platform_ref_() { }
 
   template<cl_platform_info PARAM>
   typename cl_platform_info_retval<PARAM>::value
@@ -72,7 +85,7 @@ public:
     if(err != CL_SUCCESS) throw std::runtime_error("cl error");
 
     for(int i=0; i<num_platforms; ++i) {
-      c.insert(c.end(), platform_(platform_ids[i]));
+      c.insert(c.end(), platform_ref_(platform_ids[i]));
     }
   }
 
@@ -89,7 +102,7 @@ public:
     if(err != CL_SUCCESS) throw std::runtime_error("cl error");
 
     for(int i=0; i<num_devices; ++i) {
-      c.insert(c.end(), device_<0>(device_ids[i]));
+      c.insert(c.end(), device_ref_<0>(device_ids[i]));
     }
   }
 
@@ -115,7 +128,6 @@ public:
 private:
   cl_platform_id id_;
 };
-typedef platform_<0> platform;
 
 template<cl_device_info I> struct cl_device_info_retval {
 };
@@ -170,17 +182,17 @@ device_get_info_<std::vector<std::size_t> >(cl_device_id id,
   return buffer;
 }
 
-template<> platform_<0> device_get_info_<platform_<0> >(cl_device_id id,
+template<> platform_ref_<0> device_get_info_<platform_ref_<0> >(cl_device_id id,
     cl_device_info param) {
-  return platform_<0>(device_get_info_<cl_platform_id>(id, param));
+  return platform_ref_<0>(device_get_info_<cl_platform_id>(id, param));
 }
 
 template<int UNUSED>
-class device_ {
+class device_ref_ {
 public:
-  inline device_(cl_device_id id)
+  inline device_ref_(cl_device_id id)
       : id_(id) { }
-  inline ~device_() { }
+  inline ~device_ref_() { }
 
   template<cl_device_info PARAM>
   typename cl_device_info_retval<PARAM>::value
@@ -203,195 +215,330 @@ public:
 private:
   cl_device_id id_;
 };
-typedef device_<0> device;
 
 template<int UNUSED>
-class command_queue_ : boost::noncopyable {
+class context_ref_ {
 public:
-  inline ~command_queue_() {
+  inline context_ref_(cl_context c)
+      : id_(c) {
+  }
+  inline context_ref_(const context_ref &c)
+      : id_(c.id_) {
+    clRetainContext(c.id_);
+  }
+  inline ~context_ref_() {
+    clReleaseContext(c.id_);
+  }
+
+  inline context_ref_& operator=(const context_ref_ &r) {
+    if(r.id_ != id_) {
+      clReleaseContext(id_);
+      id_ = r.id_;
+      clRetainContext(id_);
+    }
+    return *this;
+  }
+  inline bool operator==(const context_ref_ &r) const {
+    return id_ == r.id_;
+  }
+
+private:
+  cl_context id_;
+};
+
+template<int UNUSED>
+class command_queue_ref_ {
+public:
+  inline command_queue_ref_(cl_command_queue q) 
+      : id_(q) {
+  }
+  inline command_queue_ref_(const command_queue_ref_ &r) 
+      : id_(r.id_) {
+    clRetainCommandQueue(id_);
+  }
+  inline command_queue_ref_(context_ref_ context,
+    device_ref device,
+    bool out_of_order = false,
+    bool profiling = false)
+      : id_(NULL) {
+    int err;
+    id_ = clCreateCommandQueue(context.id(), device.id(),
+          (out_of_order ? CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE : 0) |
+          (profiling ? CL_QUEUE_PROFILING_ENABLE : 0)
+        &err);
+    if(err != CL_SUCCESS) {
+      throw std::runtime_error("error creating command queue");
+    }
+  }
+  inline ~command_queue_ref_() {
     clReleaseCommandQueue(id_);
   }
 
-  /* will add profiling support later 
-  bool profiling_enabled() const {
+  inline command_queue_ref_& operator=(const command_queue_ref_ &q) {
+    if(id_ != q.id_) {
+      clReleaseCommandQueue(id_);
+      id_ = q.id_;
+      clRetainCommandQueue(id_);
+    }
+    return *this;
   }
-  void set_profiling_enabled(bool b) {
-  }*/
-
-  cl_command_queue id() const {
-    return id_;
+  inline bool operator=(const command_queue_ref_ &q) {
+    return id_ == q.id_;
   }
 
 private:
-  command_queue_(cl_command_queue id)
-      : id_(id) {
-  }
-
   cl_command_queue id_;
-
-  friend class context_<0>;
 };
-typedef command_queue_<0> command_queue;
-
-void context_errfun_(const char *errinfo, const void *pinfo,
-    std::size_t cb, void *user_data);
-
-void context_errfun_noop_(const std::string &err, const void *private_info,
-    std::size_t private_size) {
-  std::cerr << "OpenCL context error: " << err << std::endl;
-}
 
 template<int UNUSED>
-class context_ : boost::noncopyable {
-public:
-  typedef boost::function<void(const std::string&, const void*, std::size_t)>
-    error_function_type;
+class event_ref_ {
+};
 
-  context_(platform p, device d)
-      : context_id_(NULL),
-      error_function_(context_errfun_noop_) {
-    devices_.push_back(d);
-    init_(p);
+template<typename T>
+class buffer_ref {
+public:
+  buffer_ref(context_ref context,
+      std::size_t num_elements,
+      bool kernel_can_read = true,
+      bool kernel_can_write = true,
+      bool mappable = false)
+      : id_(NULL) {
+    int err;
+    id_ = clCreateBuffer(context.id(),
+          ((kernel_can_read && kernel_can_write) ? CL_MEM_READ_WRITE : 0) |
+          ((!kernel_can_read && kernel_can_write) ? CL_MEM_WRITE_ONLY : 0 ) |
+          ((kernel_can_read && !kernel_can_write) ? CL_MEM_READ_ONLY : 0) |
+          (mappable ? CL_MEM_ALLOC_HOST_PTR : 0),
+        sizeof(T) * num_elements,
+        NULL,
+        &err);
+    if(err != CL_SUCCESS) {
+      throw std::runtime_error("error creating cl buffer");
+    }
+  }
+  buffer_ref(context_ref context,
+      T *buffer,
+      std::size_t num_elements,
+      bool kernel_can_read = true,
+      bool kernel_can_write = true,
+      bool mappable = false)
+      : id_(NULL) {
+    int err;
+    id_ = clCreateBuffer(context.id(),
+          ((kernel_can_read && kernel_can_write) ? CL_MEM_READ_WRITE : 0) |
+          ((!kernel_can_read && kernel_can_write) ? CL_MEM_WRITE_ONLY : 0) |
+          ((kernel_can_read && !kernel_can_write) ? CL_MEM_READ_ONLY : 0) |
+          CL_MEM_COPY_HOST_PTR |
+          (mappable ? CL_MEM_ALLOC_HOST_PTR : 0),
+        sizeof(T) * num_elements,
+        reinterpret_cast<void*>(buffer),
+        &err);
+    if(err != CL_SUCCESS) {
+      throw std::runtime_error("error creating cl buffer");
+    }
+  }
+  template<typename T2>
+  inline buffer_ref(const buffer_ref<T2> &r)
+      : id_(r.id_) {
+    clRetainMemObject(id_);
+  }
+  ~buffer_ref() {
+  }
+
+  template<typename T2>
+  bool operator==(const buffer_ref<T2> &r) const {
+    return id_ != r.id_;
+  }
+  template<typename T2>
+  buffer_ref<T>& operator=(const buffer_ref<T2> &r) {
+    if(id_ != r.id_) {
+      clReleaseMemObject(id_);
+      id_ = r.id_;
+      clRetainMemObject(id_);
+    }
+    return *this;
+  }
+
+private:
+  cl_mem id_;
+};
+
+template<int UNUSED>
+class program_ref_ {
+public:
+  inline program_ref_(cl_program p)
+      : id_(p) {
+  }
+  inline program_ref_(const program_ref_ &r)
+      : id_(r.id_) {
+    clRetainProgram(r.id_);
+  }
+  inline program_ref_(context_ref context,
+      const std::string &source)
+      : id_(NULL) {
+    int err;
+    const char *tmp = source.c_str();
+    id_ = clCreateProgramWithSource(context.id(),
+        1, &tmp,
+        NULL, &err);
+    if(err != CL_SUCCESS) {
+      throw std::runtime_error("error building CL program from source");
+    }
+  }
+  /* TODO: binary programs */
+  inline ~program_ref_() {
+    clReleaseProgram(r.id_);
+  }
+
+  inline void build(const std::string &compiler_opts) {
+    err = clBuildProgram(id_,
+        0, NULL,
+        compiler_opts.c_str(),
+        NULL, NULL);
+    if(err != CL_SUCCESS) {
+      throw std::runtime_error("error compiling CL program for all devices"
+          " in context");
+    }
   }
 
   template<typename ITER1, typename ITER2>
-  context_(platform p, ITER1 begin, ITER2 end) 
-      : context_id_(NULL),
-      error_function_(context_errfun_noop_) {
-    cl_context_properties props[] = { CL_CONTEXT_PLATFORM, p.id(), NULL };
-    devices_.insert(begin, end);
-    init_(p);
-  }
-
-  inline ~context_() {
-    std::size_t num_devices = devices_.size();
-    for(uint32_t i=0; i<num_devices; ++i) {
-      delete command_queues_[i];
+  inline void build_for(const std::string &compiler_opts,
+      ITER1 begin, ITER2 end) {
+    std::vector<device_ref> devices;
+    devices.insert(begin, end);
+    int err = clBuildProgram(id_,
+        devices.size(), reinterpret_case<cl_device_id*>(&devices[0]),
+        compiler_opts.c_str(),
+        NULL, NULL);
+    if(err != CL_SUCCESS) {
+      throw std::runtime_error("error building CL program for specific "
+          "devices");
     }
-    clReleaseContext(context_id_);
   }
 
-  inline const error_function_type& error_function() const {
-    return error_function_;
-  }
-  inline error_function_type& error_function() {
-    return error_function_;
-  }
-
-  inline std::size_t num_devices() const {
-    return devices_.size();
-  }
-  inline device devices(int i) const {
-    return devices_[i];
-  }
-  inline command_queue& command_queues(int i) const {
-    return *command_queues_[i];
-  }
-
-  inline cl_context id() const {
-    return context_id_;
-  }
-  
-private:
-  void init_(platform p) {
-    /* setup context */
+  kernel_ref get_kernel(const std::string &kernel_name) {
     int err;
-    std::size_t num_devices = devices_.size();
-    cl_context_properties props[] = { CL_CONTEXT_PLATFORM, 
-        reinterpret_cast<cl_context_properties>(p.id()), 
-        0 };
-    context_id_ = clCreateContext(props,
-        num_devices,
-        reinterpret_cast<cl_device_id*>(&devices_[0]),
-        context_errfun_,
-        this,
-        &err);
-    if(err != CL_SUCCESS) 
-      throw std::runtime_error("error creating cl context");
-
-    /* setup command queues */
-    for(uint32_t i=0; i<num_devices; ++i) {
-      cl_command_queue q;
-      q = clCreateCommandQueue(context_id_,
-          devices_[i].id(),
-          NULL,
-          &err);
-      if(err != CL_SUCCESS) {
-        throw std::runtime_error("error creating cl command queue");
-      }
-      command_queues_.push_back(new command_queue(q));
+    cl_kernel kernel_id = clCreateKernel(id_, kernel_name.c_str(), &err);
+    if(err != CL_SUCCESS) {
+      throw std::runtime_error("error creating kernel from program.  "
+          "has it been built?");
     }
+    return kernel_ref(kernel_id);
   }
 
-  cl_context context_id_;
-  std::vector<device> devices_;
-  std::vector<command_queue*> command_queues_;
-  error_function_type error_function_;
-};
-typedef context_<0> context;
+  inline program_ref_& operator=(const program_ref_ &p) {
+    if(id_ != p.id_) {
+      clReleaseProgram(id_);
+      id_ = p.id_;
+      clRetainProgram(id_);
+    }
+    return *this;
+  }
+  inline bool operator==(const program_ref_ &p) const {
+    return id_ == p.id_;
+  }
 
-void context_errfun_(const char *errinfo, const void *pinfo, 
-    std::size_t cb, void *user_data) {
-  class context_<0> *cptr = reinterpret_cast<class context_<0>*>(user_data);
-  cptr->error_function()(std::string(errinfo), pinfo, cb);
+private:
+  cl_program id_;
+};
+
+template<cl_kernel_work_group_info I>
+struct cl_kernel_workgroup_info_retval { };
+
+template<>
+struct cl_kernel_workgroup_info_retval<CL_KERNEL_WORK_GROUP_SIZE> {
+  typedef std::size_t value;
+};
+
+template<>
+struct cl_kernel_workgroup_info_retval<CL_KERNEL_COMPILE_WORK_GROUP_SIZE> {
+  typedef std::vector<std::size_t> value;
+};
+
+template<>
+struct cl_kernel_workgroup_info_retval<CL_KERNEL_LOCAL_MEM_SIZE> {
+  typedef uint64_t value;
+};
+
+template<typename RETVAL> RETVAL kernel_get_workgroup_info_(
+    cl_kernel kernel_id,
+    cl_device device_id,
+    cl_kernel_work_group_info param) {
+  RETVAL retval;
+  int err;
+  err = clGetKernelWorkGroupInfo(
+      kernel_id,
+      device_id,
+      param,
+      sizeof(RETVAL),
+      &retval,
+      NULL);
+  if(err != CL_SUCCESS) {
+    throw std::runtime_error("cl error in clGetKernelWorkGroupInfo");
+  }
+  return retval;
+}
+
+template<> std::vector<std::size_t>
+kernel_get_workgroup_info_<std::vector<std::size_t> >(
+    cl_kernel kernel_id,
+    cl_device device_id,
+    cl_kernel_work_group_info param) {
+  std::vector<std::size_t> buffer;
+  buffer.reserve(3);
+  int err;
+  err = clGetKernelWorkGroupInfo(
+      kernel_id,
+      device_id,
+      param,
+      sizeof(std::size_t)*3,
+      &buffer[0],
+      NULL);
+  if(err != CL_SUCCESS) {
+    throw std::runtime_error("cl error in clGetKernelWorkGroupInfo");
+  }
+  return buffer;
 }
 
 template<int UNUSED>
-class program_ : boost::noncopyable {
+class kernel_ref_ {
 public:
-  program_(const context &c, const std::string &source,
-    const std::string &args = "") 
-      : id_(NULL) {
-    init_(c, source, args);
+  inline kernel_ref_(cl_kernel id)
+      : id_(id) {
   }
-  ~program_() {
-    clReleaseProgram(id_);
+  inline kernel_ref_(const kernel_ref_ &k)
+      : id_(k.id_) {
+    clRetainKernel(id_);
   }
- 
-private:
-  inline void init_(const context &c, const std::string &source,
-      const std::string &args) {
-    int err;
-    const char *tmp = source.c_str();
-    id_ = clCreateProgramWithSource(c.id(),
-        1,
-        &tmp,
-        NULL,
-        &err);
-    if(err != CL_SUCCESS)
-      throw std::runtime_error("error creating cl program!");
-    err = clBuildProgram(id_,
-        0, // build for all devices
-        NULL,
-        args.c_str(),
-        NULL,
-        NULL);
-    if(err != CL_SUCCESS) {
-      switch(err) {
-        case CL_INVALID_PROGRAM:
-          throw std::runtime_error("cl: invalid program object");
-        case CL_INVALID_VALUE:
-          throw std::runtime_error("cl: invalid device config");
-        case CL_INVALID_DEVICE:
-          throw std::runtime_error("cl: invalid device");
-        case CL_INVALID_BUILD_OPTIONS:
-          throw std::runtime_error("cl: invalid build options");
-        case CL_INVALID_OPERATION:
-          throw std::runtime_error("cl: can't build for that device now");
-        case CL_COMPILER_NOT_AVAILABLE:
-          throw std::runtime_error("cl: compiler not available");
-        case CL_BUILD_PROGRAM_FAILURE:
-          throw std::runtime_error("cl: build program error");
-        case CL_OUT_OF_HOST_MEMORY:
-          throw std::runtime_error("cl: out of host memory");
-      }
-      throw std::runtime_error("error compiling cl program!");
+  inline ~kernel_ref_() { }
+
+  inline kernel_ref_& operator=(const kernel_ref_ &k) {
+    if(id_ != k.id_) {
+      clReleaseKernel(id_);
+      id_ = k.id_;
+      clRetainKernel(id_);
     }
+    return *this;
+  }
+  inline bool operator==(const kernel_ref_ &k) const {
+    return id_ == k.id_;
   }
 
-  cl_program id_;
+  template<cl_kernel_work_group_info PARAM>
+  typename cl_kernel_workgroup_info_retval<PARAM>::value
+  inline get_info(const device &d) const {
+    return kernel_get_workgroup_info_
+        <typename cl_kernel_workgrou_info_retval<PARAM>::value>
+        (id_,
+         d.id(),
+         PARAM);
+  }
+
+private:
+
+
+  cl_kernel id_;
 };
-typedef program_<0> program;
 
 }
 
